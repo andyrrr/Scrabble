7217e398ca60ac054eac00d1235a6f6119173ac0
DantrollCR <daniel-2443@hotmail.com>
2019-04-10 15:12:14 -0600

    Genera el ID
    En tablero:
    void Tablero::setIDJuego(int i){
       JuegoID = i;
    }
    int Tablero::getIDJuego(){
        return JuegoID;
    }
    en .h:
        void setIDJuego(int id);
        int getIDJuego();

--- crear.cpp
+++ crear.cpp
@@ -1,7 +1,8 @@
 #include "crear.h"
 #include "scrabble.h"
 #include "ui_crear.h"
-
+#include "tablero.h"
+int JuegoID;
 Crear::Crear(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Crear)
@@ -9,11 +10,13 @@ Crear::Crear(QWidget *parent) :
     ui->setupUi(this);
     crear_rnd();
 
+
 }
 void Crear::crear_rnd(){
     srand (time(NULL));
    int id = rand() %9999+1;
    ui->label_2->setNum(id);
+   this->JuegoID=id;
 
 }
 Crear::~Crear()
@@ -24,6 +27,8 @@ Crear::~Crear()
 void Crear::on_pushButton_clicked()
 {
     scrabble *sc = new scrabble ();
+    sc->setID(JuegoID);
     sc->show();
     this->hide();
+
 }
--- scrabble.cpp
+++ scrabble.cpp
@@ -3,16 +3,20 @@
 
 #include <QGridLayout>
 #include <QLabel>
-
+Tablero *nativo;
 scrabble::scrabble(QWidget *parent)
     :QWidget (parent)
 {
     setWindowTitle(tr("Scrabble"));
-    Tablero *nativo = new Tablero(&dibujar,this);
+     nativo = new Tablero(&dibujar,this);
     QGridLayout *layout = new QGridLayout;
     layout->addWidget(nativo,0,0);
     setLayout(layout);
 }
 scrabble::~scrabble()
 {
+}
+void scrabble::setID(int i){
+    this->id =i;
+    nativo->setIDJuego(i);
 }
